8b12f128fba07d259e0d34716a41781856d91863
Filipe Maia <filipebsmaia@gmail.com>
2019-12-26 21:19:28 -0300

    Rota de teste

--- src/routes.js
+++ src/routes.js
@@ -20,6 +20,8 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+routes.get('/', (req, res) => res.json({ status: 'ok' }));
+
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
